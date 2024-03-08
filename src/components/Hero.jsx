c7aa75feec313ce386a9769494566508726a5ddb
Abbas <meerabbas110@gmail.com>
2024-03-08 23:19:56 +0530

    initial commit side project setup for interior website

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import mainBg from '../assets/hero02-free-img.jpg';
+
+const Hero = () => {
+  return (
+    <main className="w-full">
+      <div>
+        <img src={mainBg} alt="/" />
+      </div>
+    </main>
+  );
+};
+
+export default Hero;
